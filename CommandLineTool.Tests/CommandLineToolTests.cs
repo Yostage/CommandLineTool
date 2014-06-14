c77a11e1687e82e1688cbe90faee6d19668b8f9a
Scott Yost <scott@yostage.com>
2014-06-14 14:03:57 -0700

    Add xunit. Set up nuget package restore.

---
+++ CommandLineTool.Tests/CommandLineToolTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace CommandLineTool.Tests
+{
+    public class CommandLineToolTests
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
